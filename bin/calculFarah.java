73e0b823622f24a8a3e333d3024528638ac2de1c
Farah Balloum <balloumfarah@gmail.com>
2024-04-12 20:46:10 +0200

    FarahsCommit

---
+++ bin/calculFarah.java
@@ -0,0 +1,8 @@
+public class calculFarah {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int resultat = a + b;
+        System.out.println("Le résultat de l'addition est : " + resultat);
+    }
+}
